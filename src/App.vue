25afe264b9ad6941372d2a4ed4331aa9ec80c89b
deguchi <deguchi37458@gmail.com>
2022-05-02 08:54:52 +0900

    update package.json

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="back">
-
+    <taskbar></taskbar>
+    {{name}}
   </div>
 </template>
 
 <script>
+import taskbar from './components/taskbar'
+
+export default {
+  components: {
+    taskbar
+  },
+  data: function() {
+    return {
+      name: "deguchi"
+    }
+  }
+}
+
 
 </script>
 
