85c2da7435c22948c54413d9de922a98cfc32662
deguchi37458 <deguchi37458@gmail.com>
2022-05-13 00:17:09 +0900

    update Opensource

--- src/components/views/Opensource.vue
+++ src/components/views/Opensource.vue
@@ -1,56 +1,28 @@
 <template>
 <div>
-    <h2>ｵｰﾌﾟﾝｿｰｽ</h2>
-
+    <h2>OpenSource</h2>
     <div class="inner-content">
-        <p class="paragraph">Vue.jsを用いたﾃｰﾏの作成を行なっております。</p>
-    </div>
-
-    <div class="inner-content">
-        <a href="https:/cui.deguchihi.net/" target="_blank">
-            <img src="@/assets/open-source/linux-theme.png" alt="screenshot of linux template" />
-            <h4 class="subtitle">Linux Theme</h4>
-        </a>
+        <h3 class="heading"><b>Linux Theme</b></h3>
+        <img src="@/assets/open-source/linux-theme.png" alt="screenshot of linux template" />
+        <a href="https:/cui.deguchihi.net/" target="_blank">https:/cui.deguchihi.net/</a><br>
+        <a href="https://github.com/deguchi37458/vue-cui" target="_blank">Repository</a>
     </div>
     <div class="inner-content">
-        <a href="https://windows95.deguchihi.net" target="_blank">
-            <img src="@/assets/open-source/windows95-theme.png" alt="screenshot of windows95 template" />
-            <h4 class="subtitle">Windows95 Theme</h4>
-        </a>
+        <h3 class="heading"><b>Windows95 Theme</b></h3>
+        <img src="@/assets/open-source/windows95-theme.png" alt="screenshot of windows95 template" />
+        <a href="https://windows95.deguchihi.net" target="_blank">https://windows95.deguchihi.net</a><br>
+        <a href="https://github.com/deguchi37458/windows95" target="_blank">Repository</a>
     </div>
 </div>
 </template>
 
 <style scoped>
-.self {
-    width: 100%;
-    height: auto;
-    /* border-radius: 5px; */
-}
-
-.subtitle {
-    font-size: 12px;
-    color: gray;
-}
-
 .heading {
-    padding-bottom: 20px;
-}
-
-.header {
-    padding-top: 20px;
-    padding-bottom: 20px;
+    margin-bottom: 20px;
 }
 
 .inner-content {
     padding-top: 30px;
-}
-
-h2,
-h4,
-h3 {
-    padding: 0;
-    margin: 0;
 }
 
 img {
